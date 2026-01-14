4b718508485f4c7ff2feaefc6c3441c26745b5e4
Henry Stanley <henry@usemast.com>
2026-01-14 20:28:58 +0000

    Update autoclaude to 0.0.7

--- autoclaude.rb
+++ autoclaude.rb
@@ -1,30 +1,30 @@
 class Autoclaude < Formula
   desc "Automatically resume Claude Code sessions after rate limits"
   homepage "https://github.com/henryaj/autoclaude"
-  version "0.0.6"
+  version "0.0.7"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.6/autoclaude_0.0.6_darwin_amd64.tar.gz"
-      sha256 "5f13826d97259b1406face06760e94a457abca4173d1c61a99297d555ec9a54d"
+      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.7/autoclaude_0.0.7_darwin_amd64.tar.gz"
+      sha256 "ebb34d0ad52677acb410c0c8142a00e0e609eb2c5af9c7d1029a8843e51c5758"
     end
 
     on_arm do
-      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.6/autoclaude_0.0.6_darwin_arm64.tar.gz"
-      sha256 "f2c11d822833e0e19563ef76276e248ec758d27f04d31863b425ddca8bc67fe1"
+      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.7/autoclaude_0.0.7_darwin_arm64.tar.gz"
+      sha256 "dc779b609466ed3a0ce44a01ea9d16a8319af01f3f86b9767da60bf584f9c125"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.6/autoclaude_0.0.6_linux_amd64.tar.gz"
-      sha256 "612b083b3fd232a4423bf7edaf3b8c941252f53f83c3b2d0eaf0054323546047"
+      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.7/autoclaude_0.0.7_linux_amd64.tar.gz"
+      sha256 "d1fed968cb08b2cce0f28e776cf0c2f33cc6ae184f98101f8eaed504af7c7114"
     end
 
     on_arm do
-      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.6/autoclaude_0.0.6_linux_arm64.tar.gz"
-      sha256 "b049a520f1ed8d298850253c9499544669e2239f1a70c97c84c9425b90e1605f"
+      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.7/autoclaude_0.0.7_linux_arm64.tar.gz"
+      sha256 "dfc1fba64393c5db3420a4d98aeea57349364d4c17b50375a806355ce37c05de"
     end
   end
 
