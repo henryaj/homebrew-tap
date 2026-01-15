766fe85b351dba722979d6ef9ee52f5bc79c152b
Henry Stanley <henry@usemast.com>
2026-01-15 15:45:15 +0000

    Update autoclaude to 0.1.0

--- autoclaude.rb
+++ autoclaude.rb
@@ -1,30 +1,30 @@
 class Autoclaude < Formula
   desc "Automatically resume Claude Code sessions after rate limits"
   homepage "https://github.com/henryaj/autoclaude"
-  version "0.0.7"
+  version "0.1.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.7/autoclaude_0.0.7_darwin_amd64.tar.gz"
-      sha256 "ebb34d0ad52677acb410c0c8142a00e0e609eb2c5af9c7d1029a8843e51c5758"
+      url "https://github.com/henryaj/autoclaude/releases/download/v0.1.0/autoclaude_0.1.0_darwin_amd64.tar.gz"
+      sha256 "620ed0223ad72fece24d582351351cad7dec8cc613484519630cbc9a258938fb"
     end
 
     on_arm do
-      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.7/autoclaude_0.0.7_darwin_arm64.tar.gz"
-      sha256 "dc779b609466ed3a0ce44a01ea9d16a8319af01f3f86b9767da60bf584f9c125"
+      url "https://github.com/henryaj/autoclaude/releases/download/v0.1.0/autoclaude_0.1.0_darwin_arm64.tar.gz"
+      sha256 "69b1b70db15c00e13e00a931adcb8d7af473b6eb320998b85975f1be7430c7da"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.7/autoclaude_0.0.7_linux_amd64.tar.gz"
-      sha256 "d1fed968cb08b2cce0f28e776cf0c2f33cc6ae184f98101f8eaed504af7c7114"
+      url "https://github.com/henryaj/autoclaude/releases/download/v0.1.0/autoclaude_0.1.0_linux_amd64.tar.gz"
+      sha256 "c0aa5c97036c0af34547f795a19f0a256d954df6c58f8b7610a7c1a6400ad0f0"
     end
 
     on_arm do
-      url "https://github.com/henryaj/autoclaude/releases/download/v0.0.7/autoclaude_0.0.7_linux_arm64.tar.gz"
-      sha256 "dfc1fba64393c5db3420a4d98aeea57349364d4c17b50375a806355ce37c05de"
+      url "https://github.com/henryaj/autoclaude/releases/download/v0.1.0/autoclaude_0.1.0_linux_arm64.tar.gz"
+      sha256 "7300c2cc7d3bb2791d6e06b86fb47a22074ca02899331f076cbb51a53295a8bc"
     end
   end
 
